97e7c424045361d07e616d261d2ff44c83fb1896
Colin Lin <colincool100@gmail.com>
2022-06-28 16:53:23 -0400

    quill

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css"; // ES6
+
+function App() {
+    return (
+        <div className="App">
+            <ReactQuill />
+        </div>
+    );
+}
+
+export default App;
