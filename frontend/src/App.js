462843a9c9b12ef1260e60f27b8697ae6b101300
Colin Lin <colincool100@gmail.com>
2022-06-28 18:57:17 -0400

    syntax highlighting

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,24 @@
-import logo from "./logo.svg";
+import hljs from "highlight.js";
+import javascript from "highlight.js/lib/languages/javascript";
+import python from "highlight.js/lib/languages/python";
+import "highlight.js/styles/monokai-sublime.css";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css"; // ES6
 
 function App() {
+    hljs.registerLanguage("javascript", javascript);
+    hljs.registerLanguage("python", python);
+
+    const modules = {
+        syntax: {
+            highlight: (text) =>
+                hljs.highlight(text, { language: "python" }).value,
+        },
+        toolbar: [["code-block"]],
+    };
     return (
         <div className="App">
-            <ReactQuill />
+            <ReactQuill modules={modules} />
         </div>
     );
 }
