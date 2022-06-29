e8961dc06571a3d03ce9fd5d8ac94436cfef00f4
Tony Tan <tyx1999120755@gmail.com>
2022-06-28 21:41:06 -0400

    create mongodb connection

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,31 @@
 const express = require("express");
+require("dotenv").config();
+
 const WebSocket = require("ws");
 const ShareDB = require("sharedb");
 const WebSocketJSONStream = require("@teamwork/websocket-json-stream");
 const http = require("http");
-
+const mongoose = require("mongoose");
 const app = express();
 const server = http.createServer(app);
 const webSocketServer = new WebSocket.Server({ server: server });
 
 const backend = new ShareDB();
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+mongoose
+  .connect(process.env.MONGO_CONN_STR, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((res) => {
+    console.log("connect successfully to mongo");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
 webSocketServer.on("connection", (webSocket) => {
   const stream = new WebSocketJSONStream(webSocket);
   backend.listen(stream);
