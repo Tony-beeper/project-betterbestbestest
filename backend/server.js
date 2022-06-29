b96dbd93efcafd6a82f2b361865bacf9a07c9db5
Colin Lin <colincool100@gmail.com>
2022-06-29 11:18:18 -0400

    clean up backend

--- backend/server.js
+++ backend/server.js
@@ -11,32 +11,35 @@ createDoc(startServer);
 
 // Create initial document then fire callback
 function createDoc(callback) {
-    var connection = backend.connect();
-    var doc = connection.get("code", "richtext");
-    doc.fetch(function (err) {
-        if (err) throw err;
-        if (doc.type === null) {
-            doc.create([{ insert: "Hi!" }], "rich-text", callback);
-            return;
-        }
-        callback();
-    });
+  const connection = backend.connect();
+  const doc = connection.get("code", "richtext");
+  doc.fetch((err) => {
+    if (err) throw err;
+    if (doc.type === null) {
+      // insert dummy element to initilize shardb
+      doc.create([{ insert: "Hi!" }], "rich-text", callback);
+      return;
+    }
+    callback();
+  });
 }
 
 function startServer() {
-    // Create a web server to serve files and listen to WebSocket connections
-    var app = express();
-    app.use(express.static("static"));
-    app.use(express.static("node_modules/quill/dist"));
-    var server = http.createServer(app);
+  // Create a web server to serve files and listen to WebSocket connections
+  const app = express();
+  // app.use(express.static("static"));
+  // app.use(express.static("node_modules/quill/dist"));
+  const server = http.createServer(app);
 
-    // Connect any incoming WebSocket connection to ShareDB
-    var wss = new WebSocket.Server({ server: server });
-    wss.on("connection", function (ws) {
-        var stream = new WebSocketJSONStream(ws);
-        backend.listen(stream);
-    });
+  // Connect any incoming WebSocket connection to ShareDB
+  const wss = new WebSocket.Server({ server: server });
+  wss.on("connection", function (ws) {
+    const stream = new WebSocketJSONStream(ws);
+    backend.listen(stream);
+  });
 
-    server.listen(8080);
-    console.log("Listening on http://localhost:8080");
+  const PORT = process.env.PORT || 8080;
+
+  server.listen(8080);
+  console.log("Listening on http://localhost:" + PORT);
 }
