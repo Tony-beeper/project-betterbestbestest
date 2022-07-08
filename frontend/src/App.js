bf90cf7de22d89cfd0cdc97423ad566ab1af40b6
Tony Tan <tyx1999120755@gmail.com>
2022-07-08 13:47:29 -0400

    added theme context to app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,20 +4,27 @@ import "react-toastify/dist/ReactToastify.css";
 import Bar from "./components/Navbar";
 import SignUp from "./pages/SignUp";
 import Login from "./pages/Login";
+import React, { useState } from "react";
+
+export const ThemeContext = React.createContext();
 
 function App() {
+  const [context, setContext] = useState({
+    username: "",
+  });
   return (
-    <Router>
-      <ToastContainer theme="colored" />
-      <Bar />
-      <Routes>
-        <Route path="/" element={<h1>hello world</h1>} />
-        {/* <Route path="/room/" element={<Room />} /> */}
-        {/* <Route path="/room/:id" element={<h1>room</h1>} /> */}
-        <Route path="/signup/" element={<SignUp />} />
-        <Route path="/login/" element={<Login />} />
-      </Routes>
-    </Router>
+    <ThemeContext.Provider value={[context, setContext]}>
+      <Router>
+        <ToastContainer theme="colored" />
+        <Bar />
+        <Routes>
+          <Route path="/" element={<h1>hello world</h1>} />
+
+          <Route path="/signup/" element={<SignUp />} />
+          <Route path="/login/" element={<Login />} />
+        </Routes>
+      </Router>
+    </ThemeContext.Provider>
   );
 }
 
