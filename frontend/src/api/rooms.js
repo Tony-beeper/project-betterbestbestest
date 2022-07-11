80e6efb199770cb6e35380b4da83797f557d58ac
Tony Tan <tyx1999120755@gmail.com>
2022-07-11 14:44:31 -0400

    added credentials to axios for CORs

--- frontend/src/api/rooms.js
+++ frontend/src/api/rooms.js
@@ -2,41 +2,92 @@ import axios from "axios";
 
 const backendURL = process.env.REACT_APP_BACKEND_URL + "/api/rooms";
 const createRoom = async (username, roomName) => {
-  const data = await axios.post(backendURL, {
-    username: username,
-    roomName: roomName,
+  // const data = await axios.post(backendURL, {
+  //   username: username,
+  //   roomName: roomName,
+  //   withCredentials: true,
+  // });
+
+  const res = await axios({
+    method: "POST",
+    url: backendURL,
+    headers: {},
+    data: {
+      username: username,
+      roomName: roomName,
+    },
+    withCredentials: true,
   });
-  return data.data;
+
+  return res.data;
 };
 
 const getRoom = async (roomId) => {
-  const data = await axios.get(backendURL + "/" + roomId);
+  const data = await axios.get(backendURL + "/" + roomId, {
+    withCredentials: true,
+  });
   return data.data;
 };
 
 const getRooms = async (username) => {
-  const data = await axios.get(`${backendURL}/all/${username}`);
-  return data.data;
+  // const data = await axios.get(`${backendURL}/all/${username}`, {
+  //   withCredentials: true,
+  // });
+
+  const res = await axios({
+    method: "GET",
+    url: backendURL + "/all/" + username,
+    headers: {},
+
+    withCredentials: true,
+  });
+
+  return res.data;
 };
 
 const joinRoom = async (roomNumber, joinCode) => {
-  const data = await axios.patch(`${backendURL}/join/`, {
-    roomNumber: roomNumber,
-    joinCode: joinCode,
+  const res = await axios({
+    method: "PATCH",
+    url: backendURL + "/join/",
+    headers: {},
+    data: { roomNumber: roomNumber, joinCode: joinCode },
+    withCredentials: true,
   });
-  return data.data;
+
+  return res.data;
 };
 
 const deleteRoom = async (roomId) => {
-  const data = await axios.delete(`${backendURL}/${roomId}`);
-  return data.data;
+  // const data = await axios.delete(`${backendURL}/${roomId}`, {
+  //   withCredentials: true,
+  // });
+
+  const res = await axios({
+    method: "DELETE",
+    url: backendURL + "/" + roomId,
+    headers: {},
+
+    withCredentials: true,
+  });
+
+  return res.data;
 };
 
 const leaveRoom = async (roomId) => {
-  const data = await axios.patch(`${backendURL}/leave/`, {
-    roomId: roomId,
+  // const data = await axios.patch(`${backendURL}/leave/`, {
+  //   roomId: roomId,
+  //   withCredentials: true,
+  // });
+
+  const res = await axios({
+    method: "PATCH",
+    url: backendURL + "/leave/",
+    headers: {},
+    data: { roomId: roomId },
+    withCredentials: true,
   });
-  return data.data;
+
+  return res.data;
 };
 
 const roomsAPI = {
