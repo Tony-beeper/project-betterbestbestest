34744cdf062302df7d17ac5f9bea8fbe7276c0a0
Tony Tan <tyx1999120755@gmail.com>
2022-07-12 11:59:19 -0400

    modified user validation with helper functions

--- backend/middlewares/validateUser.js
+++ backend/middlewares/validateUser.js
@@ -1,5 +1,10 @@
+const statusCode = require("../utils/StatusCodes");
+const Message = require("../utils/defaultMessages");
 const isAuthenticated = function (req, res, next) {
-  if (!req.username) return res.status(401).end("access denied");
+  if (!req.username)
+    return res
+      .status(statusCode.UNAUTHORIZED)
+      .send(Message.createErrorMessage("Access Denied"));
   next();
 };
 
