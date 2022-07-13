3494429623536c63add0e85e56d0c9d4826c4aea
Colin Lin <colincool100@gmail.com>
2022-07-13 18:51:43 -0400

    Revert "disabled samesite on user cookie"
    This reverts commit 00a79e06219d1c9b525254cd2ab082fef80ac49a.

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -36,7 +36,7 @@ router.post(
           .cookie("username", "", {
             path: "/",
             maxAge: 60 * 60 * 24 * 7,
-            // sameSite: true,
+            sameSite: true,
           })
           .status(StatusCodes.INTERNAL_SERVER_ERROR)
           .send(Message.createErrorMessage("Bcrypt Error"));
@@ -55,7 +55,7 @@ router.post(
           return res
             .cookie("username", "", {
               path: "/",
-              // sameSite: true,
+              sameSite: true,
               maxAge: 60 * 60 * 24 * 7,
             })
             .status(StatusCodes.CONFLICT)
@@ -67,7 +67,7 @@ router.post(
           return res
             .cookie("username", username, {
               path: "/",
-              // sameSite: true,
+              sameSite: true,
 
               maxAge: 60 * 60 * 24 * 7,
             })
@@ -123,7 +123,7 @@ router.post(
           return res
             .cookie("username", username, {
               path: "/",
-              // sameSite: true,
+              sameSite: true,
 
               maxAge: 60 * 60 * 24 * 7,
             })
@@ -148,7 +148,7 @@ router.get("/signout/", function (req, res, next) {
   return res
     .cookie("username", "", {
       path: "/",
-      // sameSite: true,
+      sameSite: true,
 
       maxAge: 60 * 60 * 24 * 7,
     })
