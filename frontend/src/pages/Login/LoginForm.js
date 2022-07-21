607cb77c86fc1fdfbd770d6b04f6ef22a2a49c00
Tony Tan <tyx1999120755@gmail.com>
2022-07-21 11:07:42 -0400

    modified UI of the login and sign up

--- frontend/src/pages/Login/LoginForm.js
+++ frontend/src/pages/Login/LoginForm.js
@@ -2,39 +2,75 @@ import React from "react";
 import RaisedButton from "material-ui/RaisedButton";
 import TextField from "material-ui/TextField";
 import "../LoginSignUpBox.css";
+import { Typography, Box, Button, Container, Layout } from "@material-ui/core";
 
 const LoginForm = ({ onSubmit, onChange, errors, user, type, onPwChange }) => {
   return (
-    <div className="loginBox">
-      <h1>Login</h1>
-      {errors.message && <p style={{ color: "red" }}>{errors.message}</p>}
+    <Container maxWidth="lg">
+      <div className="loginBox">
+        <Typography variant="h4">Login</Typography>
+        {errors.message && <p style={{ color: "red" }}>{errors.message}</p>}
+        <Box
+          noValidate
+          component="form"
+          onSubmit={onSubmit}
+          className="signUpForm"
+          sx={{ mt: 1 }}
+        >
+          {/* <form onSubmit={onSubmit} className="signUpForm"> */}
+          <TextField
+            autoFocus
+            required
+            fullWidth
+            name="username"
+            floatingLabelText="Username"
+            value={user.username}
+            onChange={onChange}
+            errorText={errors.username}
+            autoComplete="off"
+            label="Username"
+            margin="normal"
+          />
+          <TextField
+            required
+            fullWidth
+            type={type}
+            name="password"
+            floatingLabelText="password"
+            value={user.password}
+            onChange={onPwChange}
+            errorText={errors.password}
+            label="Password"
+            margin="normal"
+          />
 
-      <form onSubmit={onSubmit} className="signUpForm">
-        <TextField
-          name="username"
-          floatingLabelText="user name"
-          value={user.username}
-          onChange={onChange}
-          errorText={errors.username}
-        />
-        <TextField
-          type={type}
-          name="password"
-          floatingLabelText="password"
-          value={user.password}
-          onChange={onPwChange}
-          errorText={errors.password}
-        />
-
-        <br />
-        <RaisedButton
+          {/* <RaisedButton
           className="signUpSubmit"
           primary={true}
           type="submit"
           label="submit"
-        />
-      </form>
-    </div>
+        /> */}
+          <Button
+            className="signUpSubmit"
+            size="large"
+            primary={true}
+            type="submit"
+            variant="contained"
+            label="submit"
+            sx={{ mt: 3, mb: 2 }}
+            style={{
+              borderRadius: 35,
+              backgroundColor: "#b8b4ae",
+              padding: "18px 36px",
+              fontSize: "18px",
+            }}
+          >
+            Login
+          </Button>
+          {/* </form> */}
+        </Box>
+      </div>
+    </Container>
   );
 };
 
--- frontend/src/pages/Signup/SignUpForm.js
+++ frontend/src/pages/Signup/SignUpForm.js
@@ -4,6 +4,7 @@ import RaisedButton from "material-ui/RaisedButton";
 import TextField from "material-ui/TextField";
 import PasswordStr from "./PasswordStr";
 import "../LoginSignUpBox.css";
+import { Typography, Box, Button, Container, Layout } from "@material-ui/core";
 
 const SignUpForm = ({
   onSubmit,
@@ -17,65 +18,89 @@ const SignUpForm = ({
   onPwChange,
 }) => {
   return (
-    <div className="loginBox">
-      <h1>Sign Up</h1>
-      {errors.message && <p style={{ color: "red" }}>{errors.message}</p>}
+    <Container maxWidth="lg">
+      <div className="loginBox">
+        <Typography variant="h4">Sign up</Typography>
+        {errors.message && <p style={{ color: "red" }}>{errors.message}</p>}
 
-      <form onSubmit={onSubmit} className="signUpForm">
-        <TextField
-          name="username"
-          floatingLabelText="user name"
-          value={user.username}
-          onChange={onChange}
-          errorText={errors.username}
-        />
-        <TextField
-          type={type}
-          name="password"
-          floatingLabelText="password"
-          value={user.password}
-          onChange={onPwChange}
-          errorText={errors.password}
-        />
-
-        <div className="pwStrRow">
-          {score >= 1 && (
-            <div>
-              <PasswordStr score={score} />
-              <FlatButton
-                className="pwShowHideBtn"
-                label={btnTxt}
-                onClick={pwMask}
-                style={{
-                  position: "relative",
-                  left: "50%",
-                  transform: "translateX(-50%)",
-                }}
-              />
-            </div>
-          )}
-        </div>
-        <TextField
-          type={type}
-          name="pwconfirm"
-          floatingLabelText="confirm password"
-          value={user.pwconfirm}
-          onChange={onChange}
-          errorText={errors.pwconfirm}
-        />
-        <br />
-        <RaisedButton
-          className="signUpSubmit"
-          primary={true}
-          type="submit"
-          label="submit"
-        />
-      </form>
-      <p>
-        Aleady have an account? <br />
-        <a href="/login">Log in here</a>
-      </p>
-    </div>
+        <Box
+          noValidate
+          component="form"
+          onSubmit={onSubmit}
+          className="signUpForm"
+          sx={{ mt: 1 }}
+        >
+          <TextField
+            name="username"
+            floatingLabelText="user name"
+            value={user.username}
+            onChange={onChange}
+            errorText={errors.username}
+          />
+          <TextField
+            type={type}
+            name="password"
+            floatingLabelText="password"
+            value={user.password}
+            onChange={onPwChange}
+            errorText={errors.password}
+          />
+          <div className="pwStrRow">
+            {score >= 1 && (
+              <div>
+                <PasswordStr score={score} />
+                <FlatButton
+                  className="pwShowHideBtn"
+                  label={btnTxt}
+                  onClick={pwMask}
+                  style={{
+                    position: "relative",
+                    left: "50%",
+                    transform: "translateX(-50%)",
+                  }}
+                />
+              </div>
+            )}
+          </div>
+          <TextField
+            type={type}
+            name="pwconfirm"
+            floatingLabelText="confirm password"
+            value={user.pwconfirm}
+            onChange={onChange}
+            errorText={errors.pwconfirm}
+          />
+          {/* <br />
+          <RaisedButton
+            className="signUpSubmit"
+            primary={true}
+            type="submit"
+            label="submit"
+          /> */}
+          <Button
+            className="signUpSubmit"
+            size="large"
+            primary={true}
+            type="submit"
+            variant="contained"
+            label="submit"
+            sx={{ mt: 3, mb: 2 }}
+            style={{
+              borderRadius: 35,
+              backgroundColor: "#b8b4ae",
+              padding: "18px 36px",
+              fontSize: "18px",
+            }}
+          >
+            Sign Up
+          </Button>
+        </Box>
+        {/* <p>
+          Aleady have an account? <br />
+          <a href="/login">Log in here</a>
+        </p> */}
+      </div>
+    </Container>
   );
 };
 
