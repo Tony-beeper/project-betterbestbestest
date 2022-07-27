41169abf5651993e1b1b4cd177e2fadebb6659c3
Colin Lin <colincool100@gmail.com>
2022-07-27 19:29:24 -0400

    redirect when max char exceeded

--- frontend/src/components/CodeBlock/CodeBlock.js
+++ frontend/src/components/CodeBlock/CodeBlock.js
@@ -14,116 +14,116 @@ import { ThemeContext } from "../../App";
 import CodeExecution from "../CodeExecution/CodeExecution";
 import constants from "../../utils/Constants";
 function CodeBlock(props) {
-    let Nav = useNavigate();
-    const doc = props.doc;
-    const [quill, setQuill] = useState(null);
-    const [context, setContext] = useContext(ThemeContext);
-    let colors = {};
+  let Nav = useNavigate();
+  const doc = props.doc;
+  const [quill, setQuill] = useState(null);
+  const [context, setContext] = useContext(ThemeContext);
+  let colors = {};
 
-    // hljs.registerLanguage("python", python);
-    Quill.register("modules/cursors", QuillCursors);
+  // hljs.registerLanguage("python", python);
+  Quill.register("modules/cursors", QuillCursors);
 
-    useEffect(() => {
-        doc.subscribe((err) => {
-            if (err) console.log(err);
-            initQuill();
-        });
-    }, []);
+  useEffect(() => {
+    doc.subscribe((err) => {
+      if (err) console.log(err);
+      initQuill();
+    });
+  }, []);
 
-    const initQuill = () => {
-        const quill = new Quill("#editor-container", {
-            formats: ["code-block"],
-            modules: {
-                syntax: {
-                    highlight: (text) =>
-                        hljs.highlight(text, { language: "python" }).value,
-                },
-                cursors: true,
-                toolbar: false,
-            },
-            theme: "snow", // or 'bubble'
-        });
-        setQuill(quill);
-        let cursors = quill.getModule("cursors");
+  const initQuill = () => {
+    const quill = new Quill("#editor-container", {
+      formats: ["code-block"],
+      modules: {
+        syntax: {
+          highlight: (text) =>
+            hljs.highlight(text, { language: "python" }).value,
+        },
+        cursors: true,
+        toolbar: false,
+      },
+      theme: "snow", // or 'bubble'
+    });
+    setQuill(quill);
+    let cursors = quill.getModule("cursors");
 
-        quill.setContents(doc.data);
-        quill.formatLine(0, quill.getLength(), { "code-block": true });
-        quill.on("text-change", function (delta, oldDelta, source) {
-            console.log(oldDelta);
-            if (source !== "user") return;
-            console.log(delta);
-            const currLength = quill.getLength();
-            if (currLength > constants.CHAR_LIMIT) {
-                toast.error(
-                    `${currLength} exceeds max charater limit of ${constants.CHAR_LIMIT} please refresh the page.`
-                );
-                return;
-            } else {
-                doc.submitOp(delta, { source: quill });
-            }
-
-            quill.formatLine(0, quill.getLength(), { "code-block": true });
-        });
-        doc.on("op", function (op, source) {
-            if (source === quill) return;
-            quill.updateContents(op);
-            quill.formatLine(0, quill.getLength(), { "code-block": true });
-        });
-
-        doc.on("del", function (data, source) {
-            toast.error("room is deleted by the owner");
-            Nav("../room");
-        });
-
-        let presence = doc.connection.getDocPresence(
-            props.collection,
-            props.id
+    quill.setContents(doc.data);
+    quill.formatLine(0, quill.getLength(), { "code-block": true });
+    quill.on("text-change", function (delta, oldDelta, source) {
+      if (source !== "user") return;
+      const currLength = quill.getLength();
+      if (currLength > constants.CHAR_LIMIT) {
+        toast.error(
+          `${currLength} exceeds max charater limit of ${constants.CHAR_LIMIT} you will be redirected back to rooms`
         );
+        setTimeout(() => {
+          Nav("../room");
+        }, 5000);
+        quill.enable(false);
+        return;
+      } else {
+        quill.enable(true);
+        doc.submitOp(delta, { source: quill });
+      }
 
-        presence.subscribe(function (error) {
-            console.log("subscribed");
-            if (error) throw error;
-        });
+      quill.formatLine(0, quill.getLength(), { "code-block": true });
+    });
+    doc.on("op", function (op, source) {
+      if (source === quill) return;
+      quill.updateContents(op);
+      quill.formatLine(0, quill.getLength(), { "code-block": true });
+    });
 
-        presence.on("receive", function (id, range) {
-            console.log(range);
-            colors[id] = colors[id] || tinycolor.random().toHexString();
-            var name = (range && range.name) || "Anonymous";
-            cursors.createCursor(id, name, colors[id]);
-            cursors.moveCursor(id, range);
-        });
+    doc.on("del", function (data, source) {
+      toast.error("room is deleted by the owner");
+      Nav("../room");
+    });
 
-        let localPresence = presence.create();
-        console.log(presence);
+    let presence = doc.connection.getDocPresence(props.collection, props.id);
 
-        quill.on("selection-change", function (range, oldRange, source) {
-            // We only need to send updates if the user moves the cursor
-            // themselves. Cursor updates as a result of text changes will
-            // automatically be handled by the remote client.
-            if (source !== "user") return;
-            // Ignore blurring, so that we can see lots of users in the
-            // same window. In real use, you may want to clear the cursor.
-            if (!range) return;
-            // In this particular instance, we can send extra information
-            // on the presence object. This ability will vary depending on
-            // type.
-            range.name = context.username;
+    presence.subscribe(function (error) {
+      console.log("subscribed");
+      if (error) throw error;
+    });
 
-            console.log(presence);
-            localPresence.submit(range, function (error) {
-                if (error) throw error;
-                console.log(range);
-            });
-        });
-    };
+    presence.on("receive", function (id, range) {
+      console.log(range);
+      colors[id] = colors[id] || tinycolor.random().toHexString();
+      var name = (range && range.name) || "Anonymous";
+      cursors.createCursor(id, name, colors[id]);
+      cursors.moveCursor(id, range);
+    });
 
-    return (
-        <div className="code-block">
-            <UploadFileForm quill={quill} doc={doc} isCode={true} />
-            <div id="editor-container"></div>
-            <CodeExecution quill={quill} />
-        </div>
-    );
+    let localPresence = presence.create();
+    console.log(presence);
+
+    quill.on("selection-change", function (range, oldRange, source) {
+      // We only need to send updates if the user moves the cursor
+      // themselves. Cursor updates as a result of text changes will
+      // automatically be handled by the remote client.
+      if (source !== "user") return;
+      // Ignore blurring, so that we can see lots of users in the
+      // same window. In real use, you may want to clear the cursor.
+      if (!range) return;
+      // In this particular instance, we can send extra information
+      // on the presence object. This ability will vary depending on
+      // type.
+      range.name = context.username;
+
+      console.log(presence);
+      localPresence.submit(range, function (error) {
+        if (error) throw error;
+        console.log(range);
+      });
+    });
+  };
+
+  return (
+    <div className="code-block">
+      <UploadFileForm quill={quill} doc={doc} isCode={true} />
+      <div id="editor-container"></div>
+      <CodeExecution quill={quill} />
+    </div>
+  );
 }
 
 export default CodeBlock;
--- frontend/src/components/TextBlock/TextBlock.js
+++ frontend/src/components/TextBlock/TextBlock.js
@@ -8,8 +8,10 @@ import { useEffect, useState, useContext } from "react";
 import { toast } from "react-toastify";
 import { ThemeContext } from "../../App";
 import constants from "../../utils/Constants";
+import { useNavigate } from "react-router-dom";
 
 function TextBlock(props) {
+  let Nav = useNavigate();
   const doc = props.doc;
   const [quill, setQuill] = useState(null);
   const [context, setContext] = useContext(ThemeContext);
@@ -37,11 +39,20 @@ function TextBlock(props) {
     quill.setContents(doc.data);
     quill.on("text-change", function (delta, oldDelta, source) {
       if (source !== "user") return;
-      if (quill.getLength() > constants.CHAR_LIMIT) {
-        quill.deleteText(constants.CHAR_LIMIT, quill.getLength());
+      const currLength = quill.getLength();
+      if (currLength > constants.CHAR_LIMIT) {
+        toast.error(
+          `${currLength} exceeds max charater limit of ${constants.CHAR_LIMIT} you will be redirected back to rooms`
+        );
+        setTimeout(() => {
+          Nav("../room");
+        }, 5000);
+        quill.enable(false);
         return;
+      } else {
+        quill.enable(true);
+        doc.submitOp(delta, { source: quill });
       }
-      doc.submitOp(delta, { source: quill });
     });
     doc.on("op", function (op, source) {
       if (source === quill) return;
