4dadcaab48d601a27a7770412a4d923a73821311
Colin Lin <colincool100@gmail.com>
2022-07-30 13:51:06 -0400

    console output

--- frontend/src/components/CodeExecution/CodeExecution.js
+++ frontend/src/components/CodeExecution/CodeExecution.js
@@ -1,34 +1,58 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import pistonAPI from "../../api/piston";
 import ErrorHandler from "../../utils/ErrorHandler";
+import Quill from "quill";
+import "./CodeExecution.css";
+
 function CodeExecution({ quill }) {
-  const [consoleOuput, setConsoleOutput] = useState("");
-  const execute = () => {
-    const content = quill.getText();
-    console.log(content);
-    pistonAPI
-      .execute(content)
-      .then((data) => {
-        console.log(data);
-        if (data.run.code === 0) {
-          setConsoleOutput(data.run.stdout);
-        } else {
-          setConsoleOutput(data.run.stderr);
-        }
-      })
-      .catch((e) => {
-        console.error(e);
-        if (e.response) {
-          ErrorHandler.handleError(e.response);
-        }
-      });
-  };
-  return (
-    <div>
-      <button onClick={execute}>Run</button>
-      <h3>Console Output</h3>
-      <p>{consoleOuput}</p>
-    </div>
-  );
+    const [consoleOutput, setConsoleOutput] = useState(null);
+    const execute = () => {
+        const content = quill.getText();
+        console.log(content);
+        pistonAPI
+            .execute(content)
+            .then((data) => {
+                console.log(data);
+                if (data.run.code === 0) {
+                    if (consoleOutput) {
+                        consoleOutput.setText(data.run.stdout);
+                    }
+                } else {
+                    if (consoleOutput) {
+                        consoleOutput.setText(data.run.stderr);
+                    }
+                }
+            })
+            .catch((e) => {
+                console.error(e);
+                if (e.response) {
+                    ErrorHandler.handleError(e.response);
+                }
+            });
+    };
+    const initQuill = () => {
+        const consoleQuill = new Quill("#console-output-container", {
+            modules: {
+                toolbar: false,
+            },
+            theme: "snow",
+        });
+        setConsoleOutput(consoleQuill);
+        consoleQuill.on("text-change", function (delta, oldDelta, source) {
+            consoleQuill.formatLine(0, consoleQuill.getLength(), {
+                "code-block": true,
+            });
+        });
+    };
+    useEffect(() => {
+        initQuill();
+    }, []);
+    return (
+        <div>
+            <button onClick={execute}>Run</button>
+            <h3>Console Output</h3>
+            <div id="console-output-container"></div>
+        </div>
+    );
 }
 export default CodeExecution;
